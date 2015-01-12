a90283489e0d07f94201a385ae4ad96550e0e5a0
Peter Karth <falonofthetower@gmail.com>
2015-01-11 22:18:46 -0600

    partial/forms

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,10 @@
 class Post < ActiveRecord::Base
-  belongs_to :creator, :class_name => 'User', foreign_key: :creator_id
+  belongs_to :creator, class_name: 'User', foreign_key: :creator_id
   has_many :comments 
   has_many :post_categories
   has_many :categories, through: :post_categories
+
+  validates :url, presence: true
+  validates :title, presence: true, length: { minimum: 5 }
+  validates :description, presence: true, length: { minimum: 5 }
 end
