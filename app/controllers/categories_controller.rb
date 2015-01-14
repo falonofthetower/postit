b2d38b7bfbf85a25986ceb58d665fb07e8a31ea1
Peter Karth <falonofthetower@gmail.com>
2015-01-13 20:40:57 -0600

    fixing categories adding templates

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,25 @@
+class CategoriesController < ApplicationController
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    
+    if @category.save
+      redirect_to root_path
+      flash[:notice] = "Category created."
+    else
+      render :new
+    end
+  end
+
+  def show
+    @category = Category.find(params[:id])
+  end
+
+  private
+    def category_params
+      params.require(:category).permit(:name)
+    end
+end
